17fe9606047d972c48bf4662d51257ead24add8c
Anton Medbo <medbo95@gmail.com>
2022-08-27 13:13:09 +0200

    styling product row

--- lib/Components/Products/ProductRegularView.jsx
+++ lib/Components/Products/ProductRegularView.jsx
@@ -3,12 +3,10 @@ import Image from 'next/image';
 export const ProductRegularView = (props) => {
     return (
         <div className="row product">
-          <div className="product-image">
-            <Image src="/Images/4FORTY_AIR_MIPS.jpeg" width="50" height="50" />
-          </div>
+          <div className="product-image"> <Image src="/Images/4FORTY_AIR_MIPS.jpeg" width="50" height="50" /> </div>
           <div className="product-detail">
-            {props.items.Shortdescription}
-          </div>
+          <div> {props.items.Shortdescription} </div>
+         
           <div className="product-priceAndInput">
             <div className="product-price">
               <div>{props.items.Netprice}</div>
@@ -20,6 +18,7 @@ export const ProductRegularView = (props) => {
               <input className='input'></input>
             </div>
           </div>
+        </div>
         </div>
 )}
 
