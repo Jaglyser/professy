91904721254425b88e0995423783546cd0359881
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-17 15:19:46 +0200

    test

--- client/client.ts
+++ client/client.ts
@@ -1,23 +1,53 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { RestLink } from 'apollo-link-rest'
 
-const httpLink = createHttpLink({
-    uri: 'https://meta-spirit-357111.lm.r.appspot.com/graphql',
+const restLink = new RestLink({
+    // uri: 'https://meta-spirit-357111.lm.r.appspot.com/'
+    uri: 'http://localhost:8080/'
 });
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem('token');
+    const id = localStorage.getItem('id');
     return {
         headers: {
             ...headers,
             authorization: token ? `Bearer ${token}` : "",
+            "x-access-token": token ? token : '',
+            'involved-party-id': id
         }
     }
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: authLink.concat(restLink),
     cache: new InMemoryCache()
 });
+
+
+export const initClient = () => {
+    const restLink = new RestLink({
+        // uri: 'https://meta-spirit-357111.lm.r.appspot.com/'
+        uri: 'http://localhost:8080/'
+    });
+
+    const authLink = setContext((_, { headers }) => {
+        const token = localStorage.getItem('token');
+        const id = localStorage.getItem('id');
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : "",
+                "x-access-token": token ? token : '',
+                'involved-party-id': id
+            }
+        }
+    });
+    return new ApolloClient({
+        link: authLink.concat(restLink),
+        cache: new InMemoryCache()
+    })
+}
 
 export default client
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,59 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import App from '../lib/Components/index/App'
 import ProductData from '../lib/Components/Products/ProductData'
+import { gql, useQuery } from '@apollo/client'
+import client, { initClient } from '../client/client'
+import { useRouter } from 'next/router'
 
 const Home = () => {
   const jsonData = ProductData
+  const router = useRouter()
+  const ComplaintsQuery = gql`
+    query session {
+      session @rest(type: "string", path: "users") 
+    }`
+  const { data, loading, error } = useQuery(ComplaintsQuery)
+  console.log(data, error)
   return (
     <App {...jsonData}></App>
   )
 }
+
+
+
+// export async function getServerSideProps(context) {
+//   const ComplaintsQuery = gql`
+//     query session {
+//       session @rest(type: "session", path: "") 
+//     }
+//     `
+//   try {
+//     const { loading, error, data } = await client.query({
+//       query: ComplaintsQuery,
+//       context: {
+//         headers: {
+
+//         }
+//       }
+//     })
+//   } catch (err) {
+//     console.log(err)
+//     return {
+//       redirect: {
+//         destination: '/login',
+//         permanent: false,
+//       },
+//     }
+//   }
+
+//   const cpl = await fetch('https://meta-spirit-357111.lm.r.appspot.com/')
+//   console.log(cpl)
+//   return {
+//     props: {
+//       error: "oops"
+//     }, // will be passed to the page component as props
+//   }
+// }
+
 
 export default Home
