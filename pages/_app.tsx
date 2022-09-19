1f6d3111b726e0ad589896e9101f5aa81f0b1c2c
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-19 16:06:51 +0200

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '../client/client'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>Proffesy</title>
+      </Head>
       <Component {...pageProps} />
     </ApolloProvider>
   )
