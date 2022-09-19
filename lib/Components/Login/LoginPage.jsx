b3431053b2f2a3cdf0cdc2e16724d0539291bec9
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-19 17:32:21 +0200

    made it a form

--- lib/Components/Login/LoginPage.jsx
+++ lib/Components/Login/LoginPage.jsx
@@ -70,37 +70,40 @@ export const LoginPage = () => {
     return (
         <div className={classes.main}>
             <div className={classes.loginbox}>
-                <div className={classes.logo}><Image height={70} width={210} src="/Images/proffesy.png" /></div>
-                <div className={classes.text}>Proffesy delivers a nice text about what we do and and why you should use our product</div>
-                <div className={classes.logintext}>LOG IN</div>
-                <div className={classes.input}>
-                    <TextField className={classes.textfield}
-                        id="username-input"
-                        label="Username"
-                        type="username"
-                        autoComplete="current-username"
-                        size="small"
-                        onChange={(e) => updateData('username', e.target.value, values, setValues)}
-                    />
-                </div>
-                <div className={classes.input}>
-                    <TextField className={classes.textfield}
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        size="small"
-                        onChange={(e) => updateData('password', e.target.value, values, setValues)}
-                    />
+                <form onSubmit={login()}>
+                    <div className={classes.logo}><Image height={70} width={210} src="/Images/proffesy.png" /></div>
+                    <div className={classes.text}>Proffesy delivers a nice text about what we do and and why you should use our product</div>
+                    <div className={classes.logintext}>LOG IN</div>
+                    <div className={classes.input}>
+                        <TextField className={classes.textfield}
+                            id="username-input"
+                            label="Username"
+                            type="username"
+                            autoComplete="current-username"
+                            size="small"
+                            onChange={(e) => updateData('username', e.target.value, values, setValues)}
+                        />
+                    </div>
+                    <div className={classes.input}>
+                        <TextField className={classes.textfield}
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            size="small"
+                            onChange={(e) => updateData('password', e.target.value, values, setValues)}
+                        />
 
-                </div>
-                <div className={classes.loginbutton}>
-                    <Button variant="contained" onClick={() => login()} color="success">Login</Button>
-                </div>
-                <div className={classes.contact}>
-                    <p>Contact us for more information</p>
-                    <p>contact@proffesy.com</p>
-                </div>
+                    </div>
+                    <div className={classes.loginbutton}>
+                        <Button variant="contained" onClick={() => login()} color="success">Login</Button>
+                    </div>
+                    <div className={classes.contact}>
+                        <p>Contact us for more information</p>
+                        <p>contact@proffesy.com</p>
+
+                    </div>
+                </form>
             </div>
         </div>
     )
