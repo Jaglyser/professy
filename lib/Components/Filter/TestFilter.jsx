2e669ad27059474f1ebb5310866f5d62c23ea2bc
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-19 10:29:26 +0200

    added key

--- lib/Components/Filter/TestFilter.jsx
+++ lib/Components/Filter/TestFilter.jsx
@@ -8,62 +8,62 @@ import Image from 'next/image';
 
 
 export default function TestFilter() {
-  
+
   const dataFilters = ProductData;
 
-//Brand Steg 1
-  let uniqueKeys = Object.keys(dataFilters.reduce(function(result, obj) {
+  //Brand Steg 1
+  let uniqueKeys = Object.keys(dataFilters.reduce(function (result, obj) {
     return Object.assign(result, obj);
   }, {}))
 
   let uniqueModelArray = [...new Map(Object.values(dataFilters).map((item) => [item["Category"], item])).values()]
 
   const filterCategories = [
-      'Helmets',
-      'Shoes',
-      'Clothes',
-      'Bikes',
-      'Lights',
-      'Accessories'
+    'Helmets',
+    'Shoes',
+    'Clothes',
+    'Bikes',
+    'Lights',
+    'Accessories'
 
-    ];
+  ];
 
-    const secondFilterCategories = [
-      "Brand",
-      "Sub-category",
-      "Modelname",
-      "Color"
-    ];
+  const secondFilterCategories = [
+    "Brand",
+    "Sub-category",
+    "Modelname",
+    "Color"
+  ];
 
-    return (
-      <div>
+  return (
+    <div>
 
-        <div className={classes.heading}>
-          <div>Categories</div>
-          <div>Filters</div>
-        </div>
+      <div className={classes.heading}>
+        <div>Categories</div>
+        <div>Filters</div>
+      </div>
       <div className={classes.columns}>
         <div className={classes.categories}>
           {filterCategories.map((item, i) => (
-              <div className={classes.category}>
-                <div><Image src="/Images/4FORTY_AIR_MIPS.jpeg" width="40" height="40" /></div>
-                <div className={classes.text}>{item}</div>
-                </div>
+            <div key={i} className={classes.category}>
+              <div><Image src="/Images/4FORTY_AIR_MIPS.jpeg" width="40" height="40" /></div>
+              <div className={classes.text}>{item}</div>
+            </div>
           ))}
-          </div>
+        </div>
         <div>
           {secondFilterCategories.map((item, i) => (
-              <MultipleSelectChip 
-                key={i}
-                items={item}
-              />
-            ))}
-         </div>
+            <MultipleSelectChip
+              key={i}
+              items={item}
+            />
+          ))}
         </div>
-
       </div>
-      
-    )
+
+    </div>
+
+  )
 
 }
 
