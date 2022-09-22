c70ad900c64c0af0d157ea888a9dd49058b37f63
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-22 15:26:28 +0200

    upload csv function

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -1,18 +1,36 @@
 import { Input } from "@mui/material";
 import { ChangeEvent } from "react";
 import Papa from 'papaparse'
+import client from "../../client/client";
+import { gql } from "@apollo/client";
 
 export default function Upload() {
-    const onChange = (event: ChangeEvent) => {
+    const dataMutation = gql`
+    mutation sendDataMutation(
+        $data: String
+    ) {
+        credentials(input: $data)
+        @rest(type: "string", path: "read", method: "POST") {
+            data
+        }
+    }
+`
+    const sendData = async (data: any) => {
+        console.log(data)
+        try {
+            client.mutate({ mutation: dataMutation, variables: { data } })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const onChange = async (event: ChangeEvent) => {
         const target = event.target as HTMLInputElement
         if (target.files) {
             Papa.parse(target.files[0], {
-                complete: (results) => {
-                    console.log(results)
-                }
+                complete: sendData
             })
         }
-
     }
     return <div><Input type="file" onChange={onChange} /></div>
 }
